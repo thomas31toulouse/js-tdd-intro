13acf3eda9c5c47dcd678f4f76228f017741597d
thomas31toulouse <thomas-jondeau@hotmail.fr>
2021-06-12 18:59:47 +0200

    create function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,14 @@
-const assert = require('assert');
+String.prototype.toJadenCase = function () {
 
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    
+    return this.split(' ').map(capitalizeFirstLetter).join(' ');
+    console.log('i love you');
+  };
+
+const assert = require('assert');
 
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 
@@ -7,6 +16,6 @@ assert.strictEqual(capitalizeFirst.length, 1);
 
 assert.strictEqual(capitalizeFirstLetters('javaScript is love'), 'JavaScript Is Love');
 
-assert.strictEqual(capitalizeFirst('zebra'), 'Zebra');
+assert.strictEqual(capitalizeFirstLetters('zebra'), 'Zebra');
 
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirstLetters(''), '');
